f623f1df1f9bcfd065283a04a911a040b7ca99fe
Meszaros Nandor Farkas <farkas.nandor.meszaros@gmail.com>
2025-05-28 16:49:05 +0200

    Export, Import működik

--- script.js
+++ script.js
@@ -62,6 +62,63 @@ gombocska = document.createElement('button')
 gombocska.innerHTML = "Submit!!!!"
 gombocska.className = "gomb"
 form.appendChild(gombocska)
+const tbody2 = document.createElement('tbody')
+document.body.appendChild(tbody2)
+
+const exportgombocska = document.createElement('button')
+exportgombocska.textContent = 'Letoltés'
+tbody2.appendChild(exportgombocska)
+exportgombocska.addEventListener('click', () =>{
+    const link = document.createElement('a')
+    const contentaray = ['Szerzo;Szerzokorszak;Vers/Versek']
+    for(const adat of array ) {
+        contentaray.push(`${adat.szerzonev};${adat.szerzokorszak};${adat.versek}`)
+    }
+    const content = contentaray.join('\n')
+    const file = new Blob([content])
+    link.href =URL.createObjectURL(file)
+    link.download = 'newdata.csv'
+    link.click()
+    URL.revokeObjectURL(link.href)
+})
+
+const filebeillesztes = document.createElement('input')
+tbody2.appendChild(filebeillesztes); 
+filebeillesztes.id = 'fileinput' 
+filebeillesztes.type = 'file'; 
+filebeillesztes.addEventListener('change', (e) => { 
+    const file = e.target.files[0]; 
+    const fileolvaso = new FileReader(); 
+    fileolvaso.onload = () => { 
+        const filesorok = fileolvaso.result.split('\n') 
+        const fejremove = filesorok.slice(1); 
+        for(const line of fejremove){ 
+            const metszettvonal = line.trim();
+            const mezok = metszettvonal.split(';'); 
+            const adat  = { 
+                szerzonev: mezok[0],
+                szerzokorszak: mezok[1],
+                versek: mezok[2]
+            }
+            array.push(adat);
+            const tablerow = document.createElement('tr'); 
+            tbody.appendChild(tablerow); 
+            
+            const nevcell = document.createElement('td'); 
+            nevcell.textContent = adat.szerzonev;
+            tablerow.appendChild(nevcell); 
+        
+            const szerzocell = document.createElement('td');
+            szerzocell.textContent = adat.szerzokorszak;
+            tablerow.appendChild(szerzocell); 
+        
+            const vercell = document.createElement('td'); 
+            vercell.textContent = adat.versek; 
+            tablerow.appendChild(vercell); 
+        }
+    }
+    fileolvaso.readAsText(file); 
+})
 
 
 function sorhozzadas(adat) {
@@ -111,8 +168,6 @@ form.addEventListener('submit', function(e) {
         sorhozzadas(ujadat)
     }
     form.reset()
-        
-    
  
 
 });
